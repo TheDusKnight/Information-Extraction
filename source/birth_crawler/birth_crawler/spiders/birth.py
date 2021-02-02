678a5d20c07daa7bb66e6116e26f4ded985766b1
TheDusKnight <860956020@qq.com>
2021-02-02 15:40:53 -0800

    task 1 finish, try to keep 500 entries

--- source/birth_crawler/birth_crawler/spiders/birth.py
+++ source/birth_crawler/birth_crawler/spiders/birth.py
@@ -1,8 +1,6 @@
 import scrapy
-import uuid
-from datetime import datetime
 import bs4 as bs
-import sys
+
 
 class Birth(scrapy.Spider):
     name = 'birth'
@@ -21,18 +19,16 @@ class Birth(scrapy.Spider):
 
     def parse(self, response):
         self.logger.info('Index page ' + response.url)
-        next_page = response.css("div.desc a::attr(href)").get()
+        next_page = response.css("div.desc a.lister-page-next.next-page::attr(href)").get()
         content_pages = response.css("div.lister-item.mode-detail h3.lister-item-header a::attr(href)").getall()
 
-        if response.url == "https://www.imdb.com/search/name/?birth_monthday=08-01&count=100&start=101":
-            self.crawler.engine.close_spider(self, "Reached 500 bios, closing crawler now.")
-
         for page in content_pages:
-            # self.logger.info("Follow page " + page+"/bio?ref_=nm_ov_bio_sm")
-            yield response.follow(page+"/bio?ref_=nm_ov_bio_sm", callback=self.parse_page1)
+            yield response.follow(page + "/bio?ref_=nm_ov_bio_sm", callback=self.parse_page1)
 
-        if next_page is not None:
+        if next_page != "/search/name/?birth_monthday=08-01&count=100&start=601":
             yield response.follow(next_page, callback=self.parse)
+        else:
+            self.logger.info("Reached 500 page " + next_page)
 
         return
 
@@ -53,70 +49,3 @@ class Birth(scrapy.Spider):
                 }
 
         return
-
-
-    # def parse_page1(self, response):
-    #     self.logger.info('Content page ' + response.url)
-    #     uid = uuid.uuid1().hex
-    #     self.logger.info("id " + uid)
-    #
-    #     name = response.css('h1.person_title::text').get()
-    #     if not name:
-    #         name = " "
-    #     else:
-    #         name = name.strip()
-    #         if name.lower() == "tbd" or name.lower() == "tba":
-    #             name = " "
-    #
-    #
-    #     # 所有title, year, role集合
-    #     titles = response.css('table.credits.person_credits td.title.brief_metascore a::text').getall()
-    #     years = response.css('table.credits.person_credits td.year::text').getall()
-    #     roles = response.css('table.credits.person_credits td.role::text').getall()
-    #
-    #     # limits to at most 5 movies
-    #     limit = min(5, len(titles))  # if not titles, then for loop will not be executed
-    #     movies = []
-    #     for i in range(limit):
-    #         cur_title = titles[i]
-    #         cur_year = years[i]
-    #         cur_role = roles[i]
-    #
-    #         if not cur_title:
-    #             cur_title = " "
-    #         else:
-    #             cur_title = cur_title.strip()
-    #             if cur_title.lower() == "tbd" or cur_title.lower() == "tba":
-    #                 cur_title = " "
-    #
-    #         if not cur_year:
-    #             cur_year = -1
-    #         else:
-    #             cur_year = cur_year.strip()
-    #             if cur_year.lower() == "tbd" or cur_year.lower() == "tba":
-    #                 cur_year = -1
-    #             else:
-    #                 try:
-    #                     cur_year = datetime.strptime(cur_year, '%b %d, %Y').year
-    #                 except ValueError:
-    #                     self.logger.info("Date does not match pattern, set to -1")
-    #                     cur_year = -1
-    #
-    #         if not cur_role:
-    #             cur_role = " "
-    #         else:
-    #             cur_role = cur_role.strip()
-    #             if cur_role.lower() == "tbd" or cur_role.lower() == "tba":
-    #                 cur_role = " "
-    #
-    #         movies.append({"name": cur_title, "year": cur_year, "role": cur_role})
-    #
-    #     yield {
-    #         "id": uid,
-    #         "url": response.url,
-    #         "timestamp_crawl": datetime.now(),
-    #         "name": name,
-    #         "movies": movies,
-    #     }
-    #
-    #     return
